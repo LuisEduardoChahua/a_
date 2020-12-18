30934675d6edeaa4e62485c1da7a32dcfe3fca4a
LuisEduardoChahua <62721646+LuisEduardoChahua@users.noreply.github.com>
2020-12-18 07:46:48 -0500

    Update x.cpp

--- x.cpp
+++ x.cpp
@@ -13,49 +13,29 @@
 using namespace sf;
 
 class particle {
-
 private:
-
-// posición de la particula en X y en Y
     double rx_, ry_;
-// velocidad de la particula en X y en Y
     double vx_, vy_;
-// número de colisiones de la particula
     int count_;
-//radio de la particula
     double r_;
     double mass_;
     int red, gr, blu;
-
-public:
-
-
+    
+    public:
     particle(double rx, double ry, double vx, double vy,
     double r, double mass, int red, int gr, int blu):
     rx_(rx), ry_(ry), vx_(vx), vy_(vy),r_(r),
     mass_(mass), red(red), gr(gr), blu(blu){};
-
     inline int count() { return count_;}
-
     void move(double dt);
-
     void draw(sf::RenderWindow* window);
-
     double timeToHit(particle* other);
-
     double timeToHitVerticalWall();
-
     double timeToHitHorizontalWall();
-
     void bounceOff(particle* &other);
-
     void bounceOffVerticalWall();
-
     void bounceOffHorizontalWall();
-
     double kineticEnergy();
-
-
 };
 
 #endif //CLIONPROYECT_PARTICLE_H
